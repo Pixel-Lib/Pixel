69dc939d488ff6dd998eaf2bea7a8bf9d5643d53
GitHub Action <action@github.com>
2024-03-04 23:31:23 +0000

    Automatically format code

--- src/pxl/drivebase/trackingwheel.cpp
+++ src/pxl/drivebase/trackingwheel.cpp
@@ -16,24 +16,15 @@
 namespace pxl {
 
 // Constructor with encoder
-TrackingWheel::TrackingWheel(pros::ADIEncoder *encoder, float wheelDiameter,
-                             float distance, float gearRatio)
-    : encoder(encoder),
-      diameter(wheelDiameter),
-      distance(distance),
-      gearRatio(gearRatio) {}
+TrackingWheel::TrackingWheel(pros::ADIEncoder *encoder, float wheelDiameter, float distance, float gearRatio)
+    : encoder(encoder), diameter(wheelDiameter), distance(distance), gearRatio(gearRatio) {}
 
 // Constructor with rotation sensor
-TrackingWheel::TrackingWheel(pros::Rotation *rotation, float wheelDiameter,
-                             float distance, float gearRatio)
-    : rotation(rotation),
-      diameter(wheelDiameter),
-      distance(distance),
-      gearRatio(gearRatio) {}
+TrackingWheel::TrackingWheel(pros::Rotation *rotation, float wheelDiameter, float distance, float gearRatio)
+    : rotation(rotation), diameter(wheelDiameter), distance(distance), gearRatio(gearRatio) {}
 
 // Constructor with motor group
-TrackingWheel::TrackingWheel(pros::MotorGroup *motors, float wheelDiameter,
-                             float distance, float rpm)
+TrackingWheel::TrackingWheel(pros::MotorGroup *motors, float wheelDiameter, float distance, float rpm)
     : motors(motors), diameter(wheelDiameter), distance(distance), rpm(rpm) {
     motors->set_encoder_units(pros::E_MOTOR_ENCODER_ROTATIONS);
 }
@@ -44,25 +35,19 @@ bool TrackingWheel::reset() { return this->encoder->reset(); }
 // Get the distance traveled by the wheel
 float TrackingWheel::getDistanceTraveled() {
     if (this->encoder != nullptr)
-        return (float(this->encoder->get_value()) * this->diameter * M_PI /
-                360) /
-               this->gearRatio;
+        return (float(this->encoder->get_value()) * this->diameter * M_PI / 360) / this->gearRatio;
     else if (this->rotation != nullptr)
-        return (float(this->rotation->get_position()) * this->diameter * M_PI /
-                36000) /
-               this->gearRatio;
+        return (float(this->rotation->get_position()) * this->diameter * M_PI / 36000) / this->gearRatio;
     else if (this->motors != nullptr) {
         auto gearsets = this->motors->get_gearing();
         auto positions = this->motors->get_positions();
         std::vector<float> distances;
-        std::transform(positions.begin(), positions.end(), gearsets.begin(),
-                       std::back_inserter(distances),
+        std::transform(positions.begin(), positions.end(), gearsets.begin(), std::back_inserter(distances),
                        [&](double pos, pros::motor_gearset_e_t gearset) {
-                           float in =
-                               (gearset == pros::E_MOTOR_GEARSET_36)   ? 100
-                               : (gearset == pros::E_MOTOR_GEARSET_18) ? 200
-                               : (gearset == pros::E_MOTOR_GEARSET_06) ? 600
-                                                                       : 200;
+                           float in = (gearset == pros::E_MOTOR_GEARSET_36)   ? 100
+                                      : (gearset == pros::E_MOTOR_GEARSET_18) ? 200
+                                      : (gearset == pros::E_MOTOR_GEARSET_06) ? 600
+                                                                              : 200;
                            return pos * diameter * M_PI * rpm / in;
                        });
         return pxl::avg(distances);
