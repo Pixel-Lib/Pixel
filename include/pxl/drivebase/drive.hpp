344c69806d6d7b1e1ec960a9791a9d7f7c860670
GitHub Action <action@github.com>
2024-03-05 00:28:03 +0000

    Automatically format code

--- include/pxl/drivebase/drive.hpp
+++ include/pxl/drivebase/drive.hpp
@@ -1,70 +1,68 @@
 #pragma once
 
 #include <memory>
-#include "pros/rtos.hpp"
+
 #include "pros/imu.hpp"
 #include "pros/motors.hpp"
+#include "pros/rtos.hpp"
 #include "pxl/aSync.hpp"
 #include "pxl/drivebase/trackingwheel.hpp"
 #include "pxl/pid.hpp"
 
 namespace pxl {
 struct OdomSensors {
-    OdomSensors(TrackingWheel *vertical1, TrackingWheel *vertical2,
-                TrackingWheel *horizontal1, TrackingWheel *horizontal2,
-                pros::Imu *imu);
-    TrackingWheel *vertical1;
-    TrackingWheel *vertical2;
-    TrackingWheel *horizontal1;
-    TrackingWheel *horizontal2;
-    pros::IMU *imu;
+        OdomSensors(TrackingWheel *vertical1, TrackingWheel *vertical2, TrackingWheel *horizontal1,
+                    TrackingWheel *horizontal2, pros::Imu *imu);
+        TrackingWheel *vertical1;
+        TrackingWheel *vertical2;
+        TrackingWheel *horizontal1;
+        TrackingWheel *horizontal2;
+        pros::IMU *imu;
 };
 class Drivetrain {
     public:
+        Drivetrain(pros::MotorGroup *leftMotors, pros::MotorGroup *rightMotors, float trackWidth, float wheelDiameter,
+                   float rpm);
 
-    Drivetrain(pros::MotorGroup *leftMotors, pros::MotorGroup *rightMotors,
-               float trackWidth, float wheelDiameter, float rpm);
+        // Add any necessary member functions here
 
-    // Add any necessary member functions here
+        /**
+         * @brief Method to queue a function to happen mid movement
+         *
+         * @param actionPoint the type of actionPoint to be used
+         * @param input input function
+         */
+        pxl::Coord addOnAction(action_Point actionPoint, void input());
 
-    /**
-     * @brief Method to queue a function to happen mid movement
-     *
-     * @param actionPoint the type of actionPoint to be used
-     * @param input input function
-     */
-    pxl::Coord addOnAction(action_Point actionPoint, void input());
-
-    void setConstants(PID &constants);
-
+        void setConstants(PID &constants);
 
     private:
-    pros::MotorGroup *leftMotors;
-    pros::MotorGroup *rightMotors;
-    float trackWidth;
-    float wheelDiameter;
-    float rpm;
+        pros::MotorGroup *leftMotors;
+        pros::MotorGroup *rightMotors;
+        float trackWidth;
+        float wheelDiameter;
+        float rpm;
 };
 
 class ExtendedDrivetrain {
     public:
-    ExtendedDrivetrain(float verticalTrackWidth);
+        ExtendedDrivetrain(float verticalTrackWidth);
 
     private:
-    float verticalTrackWidth;
+        float verticalTrackWidth;
 };
 
-class Drivebase{
+class Drivebase {
     public:
         bool isDriverControl();
-        Drivebase(Drivetrain drivetrain,OdomSensors sensors);
-            void callibrateIMU();
+        Drivebase(Drivetrain drivetrain, OdomSensors sensors);
+        void callibrateIMU();
 
-    void callibrate(bool callibrateIMU);
+        void callibrate(bool callibrateIMU);
+
     private:
-    Drivetrain drivetrain;
-    OdomSensors sensors;
+        Drivetrain drivetrain;
+        OdomSensors sensors;
 };
-
 
 };  // namespace pxl
--- src/pxl/drivebase/drive.cpp
+++ src/pxl/drivebase/drive.cpp
@@ -1,26 +1,24 @@
 #include "pxl/drivebase/drive.hpp"
 
-
 namespace pxl {
-  // pxl::OdomSensors::sensors(TrackingWheel *vertical1,
-  //                             TrackingWheel *vertical2,
-  //                             TrackingWheel *horizontal1,
-  //                             TrackingWheel *horizontal2, pros::Imu *imu)
-  //   : vertical1(vertical1),
-  //     vertical2(vertical2),
-  //     horizontal1(horizontal1),
-  //     horizontal2(horizontal2),
-  //     imu(imu) {}
-pxl::Drivetrain::Drivetrain(pros::MotorGroup *leftMotors, pros::MotorGroup *rightMotors,
-                       float trackWidth, float wheelDiameter, float rpm)
+// pxl::OdomSensors::sensors(TrackingWheel *vertical1,
+//                             TrackingWheel *vertical2,
+//                             TrackingWheel *horizontal1,
+//                             TrackingWheel *horizontal2, pros::Imu *imu)
+//   : vertical1(vertical1),
+//     vertical2(vertical2),
+//     horizontal1(horizontal1),
+//     horizontal2(horizontal2),
+//     imu(imu) {}
+pxl::Drivetrain::Drivetrain(pros::MotorGroup *leftMotors, pros::MotorGroup *rightMotors, float trackWidth,
+                            float wheelDiameter, float rpm)
     : leftMotors(leftMotors),
       rightMotors(rightMotors),
       trackWidth(trackWidth),
       wheelDiameter(wheelDiameter),
       rpm(rpm) {}
-      pxl::Drivebase::Drivebase(Drivetrain drivetrain, OdomSensors sensors ) : drivetrain(drivetrain), sensors(sensors){
-}
-    void pxl::Drivebase::callibrate(bool callibrateIMU){
+pxl::Drivebase::Drivebase(Drivetrain drivetrain, OdomSensors sensors) : drivetrain(drivetrain), sensors(sensors) {}
+void pxl::Drivebase::callibrate(bool callibrateIMU) {
     int attempt = 1;
     bool calibrated = false;
     // calibrate inertial, and if calibration fails, then repeat 5 times or until successful
@@ -36,24 +34,25 @@ pxl::Drivetrain::Drivetrain(pros::MotorGroup *leftMotors, pros::MotorGroup *righ
         }
         // indicate error
         pros::c::controller_rumble(pros::E_CONTROLLER_MASTER, "---");
-        std::cerr<<"IMU failed to calibrate! Attempt #"<< attempt<<std::endl;
+        std::cerr << "IMU failed to calibrate! Attempt #" << attempt << std::endl;
         attempt++;
     }
     // check if its driver control through the comp switch
     if (isDriverControl() && !calibrated) {
         sensors.imu = nullptr;
-        std::cerr<<
-            "Driver control started, abandoning IMU calibration, defaulting to tracking wheels / motor encoders"<<std::endl;
+        std::cerr
+            << "Driver control started, abandoning IMU calibration, defaulting to tracking wheels / motor encoders"
+            << std::endl;
     }
     // check if calibration attempts were successful
     if (attempt > 5) {
         sensors.imu = nullptr;
-        std::cerr<<("IMU calibration failed, defaulting to tracking wheels / motor encoders")<<std::endl;
+        std::cerr << ("IMU calibration failed, defaulting to tracking wheels / motor encoders") << std::endl;
     }
-    }
-bool Drivebase::isDriverControl(){
-    return pros::competition::is_connected() && !pros::competition::is_autonomous() &&
-           !pros::competition::is_disabled();
+}
+bool Drivebase::isDriverControl() {
+    return pros::competition::is_connected() && !pros::competition::is_autonomous()
+           && !pros::competition::is_disabled();
 }
 
 }  // namespace pxl
