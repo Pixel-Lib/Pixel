358f4c80386f160f3a9520cf32d8cfe0f436faff
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-09 15:50:14 -0800

    Update seekingcontroller constructor to include Regression and globalTimeout parameters

--- include/pxl/seekingcontroller.hpp
+++ include/pxl/seekingcontroller.hpp
@@ -1,13 +1,16 @@
 #pragma once
 #include "pxl/pid.hpp"
 #include "pxl/timer.hpp"
-#include "pxl/drivebase/exit_conditions.hpp"
+#include "pxl/parametrics/regression.hpp"
 
 namespace pxl {
 class seekingcontroller {
 public:
-    seekingcontroller(PID pid);
-
+    seekingcontroller(PID pid, Regression regression, float globalTimeout);
+private:
+PID pid;
+Regression regression;
+float globalTimeout;
 };
 
 }
