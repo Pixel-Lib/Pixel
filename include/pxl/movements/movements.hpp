f093f8b5db3adaf2422a6e2555c9ff43269c0eed
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-09 11:59:24 -0800

    Add Movement class to pxl namespace

---
+++ include/pxl/movements/movements.hpp
@@ -0,0 +1,12 @@
+// Movement.hpp
+//* #include "Drive.hpp"
+//* #include "Turn.hpp"
+
+namespace pxl {
+class Movement {
+    public:
+        //* Drive drive;
+        //* Turn turn;
+        // Add other movement methods here
+};
+}
