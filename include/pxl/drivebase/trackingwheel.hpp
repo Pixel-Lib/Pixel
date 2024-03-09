8a92ff300902a1e9ec7d3baa146af6d84678eecb
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-08 19:03:53 -0800

    Add docs to trackingwheel.hpp

--- include/pxl/drivebase/trackingwheel.hpp
+++ include/pxl/drivebase/trackingwheel.hpp
@@ -17,40 +17,78 @@ namespace pxl {
  * Represents a tracking wheel on a robot.
  * Can be constructed with an encoder, rotation sensor, or motor group.
  */
+/**
+ * @brief Represents a tracking wheel used for measuring distance traveled by a robot.
+ */
 class TrackingWheel {
-    public:
-        // Constructor with encoder
-        TrackingWheel(pros::ADIEncoder *encoder, float wheelDiameter, float distance, float gearRatio);
+public:
+    /**
+     * @brief Constructor for a tracking wheel with an ADIEncoder.
+     * @param encoder Pointer to the ADIEncoder object.
+     * @param wheelDiameter Diameter of the wheel in meters.
+     * @param distance Wheel offset from the robot center in meters.
+     * @param gearRatio Gear ratio of the wheel (encoder/motor).
+     */
+    TrackingWheel(pros::ADIEncoder *encoder, float wheelDiameter, float distance, float gearRatio);
 
-        // Constructor with rotation sensor
-        TrackingWheel(pros::Rotation *rotation, float wheelDiameter, float distance, float gearRatio);
+    /**
+     * @brief Constructor for a tracking wheel with a Rotation sensor.
+     * @param rotation Pointer to the Rotation object.
+     * @param wheelDiameter Diameter of the wheel in meters.
+     * @param distance Wheel offset from the robot center in meters.
+     * @param gearRatio Gear ratio of the wheel (encoder/motor).
+     */
+    TrackingWheel(pros::Rotation *rotation, float wheelDiameter, float distance, float gearRatio);
 
-        // Constructor with motor group
-        TrackingWheel(pros::MotorGroup *motors, float wheelDiameter, float distance, float rpm);
+    /**
+     * @brief Constructor for a tracking wheel with a MotorGroup.
+     * @param motors Pointer to the MotorGroup object.
+     * @param wheelDiameter Diameter of the wheel in meters.
+     * @param distance Wheel offset from the robot center in meters.
+     * @param rpm RPM of the motor group.
+     */
+    TrackingWheel(pros::MotorGroup *motors, float wheelDiameter, float distance, float rpm);
 
-        // Reset the optocal encoder measurement
-        bool reset();
+    /**
+     * @brief Resets the optical encoder measurement.
+     * @return True if the reset was successful, false otherwise.
+     */
+    bool reset();
 
-        // Get the distance traveled by the wheel
-        float getDistanceTraveled();
+    /**
+     * @brief Gets the distance traveled by the wheel.
+     * @return The distance traveled by the wheel in meters.
+     */
+    float getDistanceTraveled();
 
-        // get the distance delta or the change in distance
-        float getDistanceDelta(bool update = true);
-        // Get the wheel offset
-        float getOffset();
+    /**
+     * @brief Gets the distance delta or the change in distance.
+     * @param update Whether to update the distance delta or not (default: true).
+     * @return The distance delta or change in distance in meters.
+     */
+    float getDistanceDelta(bool update = true);
 
-        // Get the type of wheel (0 for encoder/rotation, 1 for motor group)
-        int getType();
+    /**
+     * @brief Gets the wheel offset.
+     * @return The wheel offset from the robot center in meters.
+     */
+    float getOffset();
 
-    private:
-        std::unique_ptr<pros::ADIEncoder> encoder;  // Pointer to encoder (optional)
-        std::unique_ptr<pros::Rotation> rotation;   // Pointer to rotation sensor (optional)
-        std::unique_ptr<pros::MotorGroup> motors;   // Pointer to motor group (optional)
-        float diameter;                             // Wheel diameter in meters
-        float distance;                             // Wheel offset from the robot center in meters
-        float gearRatio;                            // Gear ratio of the wheel (encoder/motor)
-        float rpm;                                  // RPM of the motor group (optional)
-        float lastAngle = 0;                        // Last angle of the wheel
+    /**
+     * @brief Gets the type of wheel.
+     * @return 0 for encoder/rotation, 1 for motor group.
+     */
+    int getType();
+
+private:
+    std::unique_ptr<pros::ADIEncoder> encoder;  // Pointer to encoder (optional)
+    std::unique_ptr<pros::Rotation> rotation;   // Pointer to rotation sensor (optional)
+    std::unique_ptr<pros::MotorGroup> motors;   // Pointer to motor group (optional)
+    float diameter;                             // Wheel diameter in meters
+    float distance;                             // Wheel offset from the robot center in meters
+    float gearRatio;                            // Gear ratio of the wheel (encoder/motor)
+    float rpm;                                  // RPM of the motor group (optional)
+    float lastAngle = 0;                        // Last angle of the wheel
 };
 
 }  // namespace pxl
