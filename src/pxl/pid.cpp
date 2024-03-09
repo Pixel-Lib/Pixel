795e3e9ba091e340c3d211376d6e06d1e8f92e10
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-09 11:20:53 -0800

    Refactor PID class in pxl/pid.cpp

--- src/pxl/pid.cpp
+++ src/pxl/pid.cpp
@@ -1,56 +1,40 @@
 #include <chrono>
 #include <cmath>
-
+#include "pxl/pid.hpp"
 #include "pxl/util.hpp"
 
 namespace pxl {
-class PID {
-    public:
-        // Constructor
-        PID(float kP, float kI, float kD) : kP(kP), kI(kI), kD(kD) {}
 
-        // Member function to update PID
-        float update(const float error) {
-            // Calculate time difference (dt)
-            auto currentTime = std::chrono::system_clock::now();
-            std::chrono::duration<float> dt = currentTime - prevTime;
-            prevTime = currentTime;
+    // Member function to update PID
+    float PID::update(const float error) {
+        // Calculate time difference (dt)
+        auto currentTime = std::chrono::system_clock::now();
+        std::chrono::duration<float> dt = currentTime - prevTime;
+        prevTime = currentTime;
 
-            // Calculate integral
-            integral += error * dt.count();
-            if (pxl::sgn(error) != pxl::sgn(prevError)) integral = 0;
+        // Calculate integral
+        integral += error * dt.count();
+        if (pxl::sgn(error) != pxl::sgn(prevError)) integral = 0;
 
-            // Calculate derivative
-            const float derivative = (error - prevError) / dt.count();
-            prevError = error;
+        // Calculate derivative
+        const float derivative = (error - prevError) / dt.count();
+        prevError = error;
 
-            // Calculate output
-            return error * kP + integral * kI + derivative * kD;
-        }
+        // Calculate output
+        return error * kP + integral * kI + derivative * kD;
+    }
 
-        // Member function to reset PID
-        void reset() {
-            integral = 0;
-            prevError = 0;
-        }
+    // Member function to reset PID
+    void PID::reset() {
+        integral = 0;
+        prevError = 0;
+    }
 
-        // Member function to set coefficients
-        void setConstants(float kP, float kI, float kD) {
-            this->kP = kP;
-            this->kD = kD;
-            this->kI = kI;
-        }
+    // Member function to set coefficients
+    void PID::setConstants(float kP, float kI, float kD) {
+        this->kP = kP;
+        this->kD = kD;
+        this->kI = kI;
+    }
 
-    protected:
-        // Member variables
-        float kP;
-        float kI;
-        float kD;
-        float integral = 0;
-        float prevError = 0;
-
-        // Member functions can also be added here!
-    private:
-        std::chrono::time_point<std::chrono::system_clock> prevTime;
-};
 }  // namespace pxl
