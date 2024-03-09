fea614bef88f4f2f2a1618a82ab163daa5422ef2
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-08 18:40:01 -0800

    Add Coord class doxygen docs

--- include/pxl/parametrics/coord.hpp
+++ include/pxl/parametrics/coord.hpp
@@ -4,10 +4,18 @@
 
 namespace pxl {
 
+/**
+ * @brief Represents a coordinate in 2D space.
+ */
 class Coord {
     public:
         float x, y;
 
+        /**
+         * @brief Constructs a Coord object with the given x and y coordinates.
+         * @param x The x coordinate.
+         * @param y The y coordinate.
+         */
         Coord(float x, float y) : x(x), y(y) {}
 
         // Overloaded Operators
@@ -18,9 +26,33 @@ class Coord {
         Coord operator/(const float &scalar) const;
         Coord &operator+=(const Coord &other);
 
+        /**
+         * @brief Performs linear interpolation between this coordinate and another coordinate.
+         * @param other The other coordinate to interpolate towards.
+         * @param t The interpolation factor (between 0 and 1).
+         * @return The interpolated coordinate.
+         */
         Coord lerp(Coord other, float t) const;
+
+        /**
+         * @brief Calculates the Euclidean distance between this coordinate and another coordinate.
+         * @param other The other coordinate.
+         * @return The distance between the two coordinates.
+         */
         float distance(const Coord &other) const;
+
+        /**
+         * @brief Calculates the angle between this coordinate and another coordinate.
+         * @param other The other coordinate.
+         * @return The angle (in radians) between the two coordinates.
+         */
         float angle(const Coord &other) const;
+
+        /**
+         * @brief Rotates this coordinate around the origin by the specified angle.
+         * @param angle The angle (in radians) to rotate by.
+         * @return The rotated coordinate.
+         */
         Coord rotate(float angle) const;
 };
 
