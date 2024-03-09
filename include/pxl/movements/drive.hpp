b67e970df570597899bb229ea42b61e1c1e2803a
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-09 14:07:43 -0800

    Add drive distance class and seeking controllers

---
+++ include/pxl/movements/drive.hpp
@@ -0,0 +1,6 @@
+#include "pxl/drivebase/api.hpp"
+#include "pxl/pid.hpp"
+//*TODO
+/*
+? @workspace here, make a drive distance class that moves the robot a specified distance with heading correction
+*/
---
+++ include/pxl/seekingcontrollers/linear.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#include "pxl/pid.hpp"
+#include "pxl/timer.hpp"
+#include "pxl/drivebase/exit_conditions.hpp"
