c74b77e32cd72252c11bb7af9c900c1b4357e114
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-11 13:45:40 -0700

    Add normalize function docs to pxl/util.hpp

--- include/pxl/util.hpp
+++ include/pxl/util.hpp
@@ -147,7 +147,14 @@ template <typename T> T avg(const std::vector<T> &vec) {
     for (const T &elem : vec) { sum += elem; }
     return sum / vec.size();
 }
-
+/**
+ * @brief normalizes the output of a lateral and angular controller so speeds do not exceed the `maxSpeed`
+ * 
+ * @param lateralOut 
+ * @param angularOut 
+ * @param maxSpeed 
+ * @return std::pair<float,float> 
+ */
 std::pair<float,float> normalize(float lateralOut, float angularOut, float maxSpeed);
 
 }  // namespace pxl
