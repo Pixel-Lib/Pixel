b4f7281f96b278db32e6a13717d46155b62a4ce3
GitHub Action <action@github.com>
2024-03-11 19:36:19 +0000

    Automatically format code

--- include/pxl/movements/drive.hpp
+++ include/pxl/movements/drive.hpp
@@ -5,21 +5,20 @@
 #include "pxl/pid.hpp"
 namespace pxl {
 class Drive_ {
-        private:
-    Drivebase drivebase;
-    struct Params{
-        float minSpeed = 0;
-        float maxSpeed = 127;
-        float slew = NAN;
-    }; 
-    Params* params;
-    // get the current competition state. If this changes, the movement will stop
-    uint8_t compstate = pros::competition::get_status();
-    friend class Drivebase;
-    public: 
-    void Drive(float target, float timeout, Params* params, bool async=true);
-    
+    private:
+        Drivebase drivebase;
+        struct Params {
+                float minSpeed = 0;
+                float maxSpeed = 127;
+                float slew = NAN;
+        };
+        Params *params;
+        // get the current competition state. If this changes, the movement will stop
+        uint8_t compstate = pros::competition::get_status();
+        friend class Drivebase;
 
-    };
-    
-}
+    public:
+        void Drive(float target, float timeout, Params *params, bool async = true);
+};
+
+}  // namespace pxl
