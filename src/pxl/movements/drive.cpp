239a0e87d328fe9135c7af538ca9b566b4e12e78
GitHub Action <action@github.com>
2024-03-11 19:10:38 +0000

    Automatically format code

--- src/pxl/movements/drive.cpp
+++ src/pxl/movements/drive.cpp
@@ -1,21 +1,23 @@
 #include "pxl/movements/drive.hpp"
+
 #include <sys/_stdint.h>
+
 #include "pros/misc.hpp"
 #include "pxl/drivebase/drive.hpp"
-#include "pxl/parametrics/pose.hpp"
-#include "pxl/parametrics/coord.hpp"
 #include "pxl/drivebase/odom.hpp"
+#include "pxl/parametrics/coord.hpp"
+#include "pxl/parametrics/pose.hpp"
 // TODO drivebase.drivetrain.leftMotors->move(127);
-namespace pxl{
-    void Drive_::Drive(float target, float timeout, Params* params, bool async){
-        if (async){
-            pros::Task task([&](){Drive(target, timeout, params, true);});
-            // end current motion just in case
-            pros::delay(10);
-            return;
-        }
-        Pose targetPose = drivebase.odom.getPose();
-        targetPose +=  targetPose.rotate(drivebase.odom.getPose().theta) * target;
+namespace pxl {
+void Drive_::Drive(float target, float timeout, Params *params, bool async) {
+    if (async) {
+        pros::Task task([&]() { Drive(target, timeout, params, true); });
+        // end current motion just in case
+        pros::delay(10);
+        return;
     }
-
+    Pose targetPose = drivebase.odom.getPose();
+    targetPose += targetPose.rotate(drivebase.odom.getPose().theta) * target;
 }
+
+}  // namespace pxl
