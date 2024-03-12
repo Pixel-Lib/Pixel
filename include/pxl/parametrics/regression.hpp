c86078bcd922fc45c182a64f1dfa4cdcf40a8211
GitHub Action <action@github.com>
2024-03-12 04:03:00 +0000

    Automatically format code

--- include/pxl/parametrics/regression.hpp
+++ include/pxl/parametrics/regression.hpp
@@ -1,15 +1,16 @@
 
 
-#include <vector>
 #include <utility>
+#include <vector>
+
 #include "pxl/parametrics/coord.hpp"
 namespace pxl {
 
-    // Define a pair of doubles as a Point
-    // typedef std::pair<double, double> Point;
+// Define a pair of doubles as a Point
+// typedef std::pair<double, double> Point;
 
-    // Define the Regression class
-    class Regression {
+// Define the Regression class
+class Regression {
     private:
         std::vector<pxl::Coord> points;
         double lambda;
@@ -18,14 +19,12 @@ namespace pxl {
         // Constructor for the Regression class
         Regression(std::vector<pxl::Coord> points);
 
-
-
         // Function to calculate the dot product of two vectors
-        double dotProduct(const std::vector<double>& v1, const std::vector<double>& v2);
+        double dotProduct(const std::vector<double> &v1, const std::vector<double> &v2);
 
         // Function to fit a function through the coordinates using ridge regression
         std::pair<double, double> Ridge();
 
         double predict(double x);
-    };
-}
+};
+}  // namespace pxl
