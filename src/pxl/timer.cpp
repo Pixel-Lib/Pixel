b105abc2326bf4e10cc3419c1f5f0dd953da35eb
GitHub Action <action@github.com>
2024-03-14 07:59:37 +0000

    Automatically format code

--- src/pxl/timer.cpp
+++ src/pxl/timer.cpp
@@ -6,9 +6,7 @@
 
 namespace pxl {
 // Constructor
-Timer::Timer(uint32_t stop_time) {
-    this->stop_time = (stop_time != -1) ? stop_time + pros::millis() : -1;
-}
+Timer::Timer(uint32_t stop_time) { this->stop_time = (stop_time != -1) ? stop_time + pros::millis() : -1; }
 
 // Destructor
 Timer::~Timer() {
@@ -21,13 +19,9 @@ void Timer::start() { start_time = pros::millis(); }
 void Timer::stop() { this->start_time = -1; }
 
 // Get the elapsed time since starting the timer
-uint32_t Timer::get_elapsed_time() {
-    return (start_time == -1) ? -1 : pros::millis() - start_time;
-}
+uint32_t Timer::get_elapsed_time() { return (start_time == -1) ? -1 : pros::millis() - start_time; }
 
-uint32_t Timer::get_time_left() {
-    return (stop_time == -1) ? -1 : stop_time - pros::millis();
-}
+uint32_t Timer::get_time_left() { return (stop_time == -1) ? -1 : stop_time - pros::millis(); }
 
 bool Timer::isDone() { return Timer::get_time_left() <= 0; }
 
