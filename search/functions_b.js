26ac1bf30a47ca96ba65b9adf0716cef969ca149
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-25 04:42:41 +0000

    Deploying to gh-pages from @ Pixel-Lib/Pixel@3236dfaf2810049cbcd2087d43771fb0551548e0 🚀

--- search/functions_b.js
+++ search/functions_b.js
@@ -6,7 +6,7 @@ var searchData=
   ['setstandstill_295',['setStandStill',['../structpxl_1_1stand__still.html#ae8204f83178b92b936dff72c17591d8d',1,'pxl::stand_still']]],
   ['sgn_296',['sgn',['../namespacepxl.html#afa79c20911ee3116206f645a9e1e3ed0',1,'pxl']]],
   ['slew_297',['slew',['../namespacepxl.html#aef4d585e132a7669441e66bd34be8f11',1,'pxl']]],
-  ['slewspeedlimits_298',['slewSpeedLimits',['../classpxl_1_1Drivebase.html#a7e92c733522dd866f1b09d2ce81f12aa',1,'pxl::Drivebase::slewSpeedLimits()'],['../namespacepxl.html#a0125ead4f4c1dd9440418dafa2b78039',1,'pxl::slewSpeedLimits()']]],
+  ['slewspeedlimits_298',['slewSpeedLimits',['../classpxl_1_1Drivebase.html#a7e92c733522dd866f1b09d2ce81f12aa',1,'pxl::Drivebase']]],
   ['start_299',['start',['../classpxl_1_1Timer.html#a83a51e3476cdc86dfbbffd15260812c4',1,'pxl::Timer']]],
   ['stop_300',['stop',['../classpxl_1_1Timer.html#a613935d6032016e937848b8493283ea8',1,'pxl::Timer']]]
 ];
