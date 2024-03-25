456402ee70c8fa7910c3a2907b9fb49db20d2d54
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-24 22:17:37 -0700

    Fix Boomerang function signature

--- src/pxl/movements/boomerang.cpp
+++ src/pxl/movements/boomerang.cpp
@@ -1,11 +1,7 @@
 #include "pxl/drivebase/drivebase.hpp"
 
 namespace pxl {
+    void Drivebase::Boomerang(float x, float y, float theta, float timeout, std::shared_ptr<boomerangParams> boomerangParams, bool async) {
 
-void Drivebase::Boomerang(float x, float y, float theta, float dlead, float timeout, std::shared_ptr<boomerangParams> boomerangParams, bool async) {
-    // This is a placeholder for the boomerang movement
-    // This will be implemented in the future
+    }  
 }
-
-}  
-
