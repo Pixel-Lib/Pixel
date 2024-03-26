784ddc10aa07b411a26631d25a346bbe4f18386f
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-26 08:13:34 -0700

    Fix boomerang movement direction

--- src/pxl/movements/boomerang.cpp
+++ src/pxl/movements/boomerang.cpp
@@ -99,6 +99,9 @@ void Drivebase::Boomerang(float x, float y, float theta, float timeout,
         // if the carrot has settled, reduce the linear output by the cosine of the angular error
         if (carrotSettled) linearOutput *= std::cos(degToRad(angularError));
 
+        //
+        if (boomerangParams->forward) linearOutput = -linearOutput;
+
         // calculate and normalize the left/right speeds
         std::pair<float, float> normalized = normalize(linearOutput, angularOutput, maxSpeed);
 
