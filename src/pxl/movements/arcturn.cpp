01ffd3862262d251184a3036be73ce0c0cbd13d9
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-26 11:15:07 -0700

    Add async support to Arcturn method

--- src/pxl/movements/arcturn.cpp
+++ src/pxl/movements/arcturn.cpp
@@ -2,8 +2,13 @@
 
 namespace pxl {
 
-void Arcturn(float target, float radius, float timeout){
-
+void Drivebase::Arcturn(float target, float radius, float timeout, std::shared_ptr<arcturnParams> params, bool async){
+    mutex.take(TIMEOUT_MAX);
+    if (async) {
+        pros::Task task([&]() { Arcturn(target, radius, timeout, params, false); });
+        pros::delay(10);
+        return;
+    }
 }
 
 }
