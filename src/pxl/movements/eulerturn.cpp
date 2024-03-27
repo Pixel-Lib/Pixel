23f5ad3b2ed1c2770f92d537ccc4e8951fac1292
Raptorly1 <95979895+Raptorly1@users.noreply.github.com>
2024-03-27 09:07:05 -0700

    eulerTurn movement added

--- src/pxl/movements/eulerturn.cpp
+++ src/pxl/movements/eulerturn.cpp
@@ -10,6 +10,48 @@ void Drivebase::eulerTurn(float target, float rate, float timeout, eulerTurnPara
         return;
     }
 
+    float curvature = 0.0f;
+
+        // start the timeout
+    Timer localTimeout(timeout);
+    localTimeout.start();
+    linearController.timerStart();
+
+    while (!localTimeout.isDone() || !linearController.getExit(error)){
+        curvature+=rate;
+
+        float curr = this->odom.getPose().theta;
+
+        float sl = target * (1/curvature + this->drivetrain.trackWidth);
+        float sr = target * (1/curvature - this->drivetrain.trackWidth);
+        float ratio = sl/sr;
+
+        float vel = this->linearController.update(angleError(target, curr, false));
+        float minSpeed = params.minSpeed;
+        float maxSpeed = params.maxSpeed;
+
+        std::pair<float, float> speeds = this->slewSpeedLimits(params, this->linearController);
+        minSpeed = speeds.first;
+        maxSpeed = speeds.second;
+
+        vel = std::fabs(vel) >= params.maxSpeed ? (params.maxSpeed * pxl::sgn(vel)) : vel;
+        float rvel = (2*vel)/(ratio+1);
+        float lvel = ratio*rvel;
+
+std::pair<float, float> normalized = normalize(lvel, rvel, maxSpeed, true);
+
+                if (params.dir == 1) {
+            drivetrain.leftMotors->move(normalized.first);
+            drivetrain.rightMotors->move(normalized.second);
+        } else {
+            drivetrain.leftMotors->move(-normalized.second);
+            drivetrain.rightMotors->move(-normalized.first);
+        }
+        
+pros::delay(10);
+    }
+        drivetrain.leftMotors->move(0);
+    drivetrain.rightMotors->move(0);
 }
 
 }  // namespace pxl
